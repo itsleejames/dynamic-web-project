a459bd20e6e28bdc084a6159f02b005a3f19b7be
Lee James <k00297799@student.tus.ie>
2024-10-25 13:51:54 +0100

    adding files
    adding files

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById("fetchDataButton",onclick(myFunction()));
+
+
+function myFunction(){
+
+
+
+    fetch(`http://picsum.photos/400/300?random=${Math.random()}`)
+
+
+
+
+    // you would first select the button by ID
+    // You would then create a function
+    // you would then insert the code to display the random image from the link provided
+    // you would then create a div in the html document where the displayed image would be shown
+    // you would then write the code to animate the image
+
+
+
+
+}
+
+
