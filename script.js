80991e7bdb0fb4ade7481b86caaabee10218fbdb
Lee James <k00297799@student.tus.ie>
2024-10-25 13:55:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,6 +15,10 @@ function myFunction(){
     // you would then insert the code to display the random image from the link provided
     // you would then create a div in the html document where the displayed image would be shown
     // you would then write the code to animate the image
+    // you would add eventListener to the button also
+    // then add a functiom
+    //in this function write code that will change the background color
+    //psuh and commit these changes to github
 
 
 
